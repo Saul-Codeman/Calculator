f642e9e3eaf7acedd6c5455a05931fd370166e07
Sean Roberts <seanarmandroberts@outlook.com>
2023-07-13 13:53:35 -0400

    Finished UI

---
+++ javascript.js
@@ -0,0 +1,43 @@
+function add(num1 , num2) {
+    return num1 + num2;
+}
+
+function subtract(num1 , num2) {
+    return num1 - num2;
+}
+
+function multiply(num1 , num2) {
+    return num1 * num2;
+}
+
+function divide(num1 , num2) {
+    return num1 / num2;
+}
+
+// Clears the answer-container
+function clear() {
+
+}
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Gets the number buttons and adds event listeners to them
+    const numberButtons = document.querySelectorAll('.number-buttons');
+    numberButtons.forEach(number => {
+        number.addEventListener('click', () => {
+            // Store the clicked number in some variable
+        });
+    });
+
+
+
+
+});
